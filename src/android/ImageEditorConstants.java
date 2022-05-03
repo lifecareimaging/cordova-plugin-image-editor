50dee561800174af6787c21fe9c684091513d329
Marko Pulkkinen <marko.pulkkinen@tietoevry.com>
2022-05-03 09:02:54 +0300

    Change plugin fileprovider authorities to not conflict with other similar file providers

--- src/android/ImageEditorConstants.java
+++ src/android/ImageEditorConstants.java
@@ -2,7 +2,7 @@ package com.lifecare.cordova.editor;
 
 public class ImageEditorConstants {
     public static final int IMAGE_URL_CODE = 1001;
-    public static final String PROVIDER_EXTENSION = ".provider";
+    public static final String PROVIDER_EXTENSION = ".editor.provider";
     public static final String FILE_PREFIX = "file://";
     public static final String APPLICATION_ID_KEY = "APPLICATION_ID";
     public static final String GOOGLE_PHOTOS_PACKAGE_NAME = "com.google.android.apps.photos";
