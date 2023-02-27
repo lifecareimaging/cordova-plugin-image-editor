69a5ed9465fe8e34e9f26388f0d3103498bdce60
Lauri Miettinen <lauri.miettinen@tietoevry.com>
2023-02-27 10:24:55 +0200

    Implement Android SDK level 31 support

--- src/android/FileProvider.java
+++ src/android/FileProvider.java
@@ -1,3 +1,3 @@
 package com.lifecare.cordova.editor;
 
-public class FileProvider extends android.support.v4.content.FileProvider {}
+public class FileProvider extends androidx.core.content.FileProvider {}
--- src/android/ImageEditor.java
+++ src/android/ImageEditor.java
@@ -9,7 +9,7 @@ import com.lifecare.cordova.editor.ImageEditorConstants.ImageEditorAction;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 
 import org.apache.cordova.BuildHelper;
 import org.apache.cordova.CallbackContext;
